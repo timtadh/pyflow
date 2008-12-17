5c9cb6ca937a0191f4cdfb5f6c9af1ee23f22e4c
Tim Henderson <tim.tadh@gmail.com>
2008-12-16 19:04:31 -0500

    TypedefTable finished
    Tim Henderson <tim.tadh@gmail.com>

--- table.py
+++ table.py
@@ -17,7 +17,26 @@ class SymbolTable(object):
         '''find_symbol(name):
             name = c_types.Identifier.name whatever was in the name field of the id you are 
                    looking for
-           returns c_types.Identifier or None if the symbols was not found'''
+           returns c_types.Identifier or None if the symbol was not found'''
+        if self.table.has_key(name): return self.table[name]
+        else: return None
+
+class TypedefTable(object):
+    
+    def __init__(self):
+        self.table = dict()
+    
+    def create_type(self, type):
+        '''create_type(type):
+            type = one of the types from the c_types module'''
+        if self.table.has_key(type.type_name):
+            raise Exception, 'type "%s" was already in table' % type.type_name
+        self.table.update({type.type_name:type})
+    
+    def find_type(self, name):
+        '''find_type(name):
+            name = type_name
+           returns a Type from c_types or None if the type was not found'''
         if self.table.has_key(name): return self.table[name]
         else: return None
 
@@ -34,3 +53,16 @@ if __name__ == '__main__':
     del g
     h = symbol_table.find_symbol('main')
     print h
+    
+    typedef_table = TypedefTable()
+    t = c_types.ScalarType('int', 4)
+    typedef_table.create_type(t)
+    print t
+    del t
+    v = typedef_table.find_type('int')
+    print v
+    v.size = 8
+    print v
+    del v
+    w = typedef_table.find_type('int')
+    print w
